d307197f2225e401fe8a4539762379417cfb68c2
Luna <git@l4.pm>
2021-09-26 00:15:44 -0300

    add logging to a file and signal handling

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,24 @@ const c = @cImport({
 
 const logger = std.log.scoped(.maid);
 
+var logfile_optional: ?std.fs.File = null;
+
+// based on the default std.log impl
+pub fn log(
+    comptime message_level: std.log.Level,
+    comptime scope: @Type(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const level_txt = comptime message_level.asText();
+    const prefix2 = if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
+    // if no logfile, prefer stderr
+    const stream = if (logfile_optional) |logfile| logfile.writer() else std.io.getStdErr().writer();
+    const held = std.debug.getStderrMutex().acquire();
+    defer held.release();
+    nosuspend stream.print(level_txt ++ prefix2 ++ format ++ "\n", args) catch return;
+}
+
 fn draw_movable_box(nc: *c.ncplane) !*c.ncplane {
     var nopts = std.mem.zeroes(c.ncplane_options);
     nopts.y = 5;
@@ -44,8 +62,52 @@ const CursorState = struct {
 
 const NOTCURSES_U32_ERROR = 4294967295;
 
+var zig_segfault_handler: fn (i32, *const std.os.linux.siginfo_t, ?*const c_void) callconv(.C) void = undefined;
+
+fn quit_handler(signal: c_int, info: *const std.os.linux.siginfo_t, uctx: ?*const c_void) callconv(.C) void {
+    if (signal == std.os.linux.SIG.SEGV) {
+        zig_segfault_handler(signal, info, uctx);
+    } else {
+        logger.info("exiting! {d}", .{signal});
+        std.os.exit(1);
+    }
+}
+
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const logfile_path = std.os.getenv("LOGFILE");
+    if (logfile_path != null) {
+        logfile_optional = try std.fs.cwd().createFile(
+            logfile_path.?,
+            .{ .truncate = true, .read = false },
+        );
+    }
+
+    defer {
+        if (logfile_optional) |logfile| logfile.close();
+    }
+
+    // configure signals
+
+    var mask = std.os.linux.empty_sigset;
+    std.os.linux.sigaddset(&mask, std.os.SIG.TERM);
+    std.os.linux.sigaddset(&mask, std.os.SIG.INT);
+    var sa = std.os.Sigaction{
+        .handler = .{ .sigaction = quit_handler },
+        .mask = mask,
+        .flags = 0,
+    };
+
+    var old_sa: std.os.Sigaction = undefined;
+
+    _ = std.os.linux.sigaction(std.os.linux.SIG.SEGV, &sa, &old_sa);
+    zig_segfault_handler = old_sa.handler.sigaction.?;
+    _ = std.os.linux.sigaction(std.os.linux.SIG.TERM, &sa, null);
+    _ = std.os.linux.sigaction(std.os.linux.SIG.INT, &sa, null);
+
+    std.log.info("boot!", .{});
+    while (true) {
+        std.time.sleep(1 * std.time.ns_per_s);
+    }
     var nopts = std.mem.zeroes(c.notcurses_options);
     nopts.flags = c.NCOPTION_NO_ALTERNATE_SCREEN;
     nopts.flags |= c.NCOPTION_SUPPRESS_BANNERS;
@@ -72,9 +134,15 @@ pub fn main() anyerror!void {
     while (true) {
         var inp: c.ncinput = undefined;
         const character = c.notcurses_getc_blocking(nc, &inp);
+        logger.info("input {}", .{inp});
         if (character == NOTCURSES_U32_ERROR) {
             logger.err("Error: {s}", .{c.strerror(std.c._errno().*)});
             return error.FailedToGetInput;
+        }
+
+        if (inp.ctrl and inp.id == 'c') {
+            std.debug.warn("AA\n", .{});
+            break;
         }
 
         //const plane_x = c.ncplane_x(plane);
